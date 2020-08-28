3cc08b9cc3852dc639ca126dc5cded9a8d9ef8cd
gabrieljakubek <gabrieljakubek@gmail.com>
2020-08-27 23:12:58 -0300

    Septimo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'herokutest2';
+  public edadUno: number = 0;
+  public edadDos: number = 0;
+  public resultado: number = 0;
+  public promedio: number = 0;
+  constructor() {}
+  calcular(): any {
+    this.resultado = this.edadUno + this.edadDos;
+    this.promedio = this.resultado / 2;
+  }
+  limpiar(): any {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.resultado = 0;
+    this.promedio = 0;
+  }
 }
